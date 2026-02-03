6abea71edc2d7485120987bc718eafdce348b245
Ethan Voraritskul <contactethanv@gmail.com>
2026-02-03 13:18:15 -0600

    made the player hold and drop treats
    fixed fruit to treats to be accurate to sprite style, made spacebar drop treats, made treats not fly into the sky after bouncing by fixing gravity

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -4,7 +4,7 @@ using UnityEngine.InputSystem;
 public class PlayerBehaviour : MonoBehaviour
 {
     public float speed;
-    public GameObject fruit;
+    public GameObject treat;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -25,5 +25,18 @@ public class PlayerBehaviour : MonoBehaviour
         Vector3 newPosition = transform.position;
         newPosition.x = transform.position.x + update;
         transform.position = newPosition;
+
+        if (treat != null) {
+            Vector3 offset = new Vector3(0, -1.0f, 0);
+            treat.transform.position = transform.position + offset;
+            treat.GetComponent<Rigidbody2D>().gravityScale = 0.0f;
+            treat.GetComponent<Collider2D>().enabled = false;
+        }
+
+        if (Keyboard.current.spaceKey.wasPressedThisFrame) {
+            treat.GetComponent<Rigidbody2D>().gravityScale = 1.0f;
+            treat.GetComponent<Collider2D>().enabled = true;
+            treat = null;
+        }
     }
 }
