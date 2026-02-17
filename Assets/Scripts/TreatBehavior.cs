1ff5d67a768a3e3f36e961257816ec07682c836e
Ethan Voraritskul <contactethanv@gmail.com>
2026-02-17 15:14:05 -0600

    added fruit merging
    implemented fruit merging using an array that increases type by one on collide, deletes old 2 fruit

--- Assets/Scripts/TreatBehavior.cs
+++ Assets/Scripts/TreatBehavior.cs
@@ -3,25 +3,84 @@ using UnityEngine;
 
 public class TreatBehavior : MonoBehaviour
 {
+    public GameObject[] treats;
+    public int treatType;
+    private bool isMerging = false; //make so only one can merge
+    
+    
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        treats = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerBehaviour>().treats;
+    }
     
     
     
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Treat")
+        {
+            int otherType = other.gameObject.GetComponent<TreatBehavior>().treatType;
+            
+            
+            if(otherType == treatType && treatType < treats.Length -1 && !isMerging)
+            {
+                isMerging = true; //set self to merging
+                other.gameObject.GetComponent<TreatBehavior>().isMerging = true; //set other to merging
+
+                //get spawn position (contact point)
+                Vector2 testSpawnPos = other.contacts[0].point;
+                
+                
+                
+                
+                
+                //create merged one
+                int choice = treatType + 1;
+                GameObject newTreat = Instantiate(treats[choice], testSpawnPos, Quaternion.identity);
+                newTreat.GetComponent<Collider2D>().enabled = true;
+                newTreat.GetComponent<Rigidbody2D>().gravityScale = 1;
+                
+                
+                //destroy other
+                Destroy(other.gameObject);
+                //destroy self
+                Destroy(gameObject);
+
+
+
+
+            }
+
+            
+                
+        }
+        
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     // public float timeout;
     // public float timeStart;
     //
@@ -88,5 +147,5 @@ public class TreatBehavior : MonoBehaviour
     //     
     // }
     //
-    
+
 }
