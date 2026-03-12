e59045538a61d47caefbd8cd4abd5593d136f1f0
Ethan Voraritskul <contactethanv@gmail.com>
2026-03-12 15:12:04 -0500

    added sickening background
    added background that makes me sick and moves

---
+++ Assets/Scripts/BackgroundManager.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class BackgroundManager : MonoBehaviour
+{
+
+    public GameObject backPrefab;
+
+    public float speed;
+
+    private GameObject[] backgrounds;
+
+    public float pivotPoint;
+    public float scale;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        pivotPoint = scale * 16 * -0.32f;
+        backPrefab.transform.localScale = new Vector3(scale,scale,scale);
+        
+        
+        
+        
+        backgrounds = new GameObject[3];
+        for (int i = 0; i < 3; i++)
+        {
+            float xPos = pivotPoint - (pivotPoint / 2 * i);
+            float yPos = pivotPoint - (pivotPoint / 2 * i);
+            Vector2 position = new Vector2(xPos, yPos);
+            backgrounds[i] = Instantiate(backPrefab, position, Quaternion.identity);
+        }
+
+    }
+    
+    void Update()
+    {
+        
+        for (int i = 0; i < 3; i++)
+        {
+            float xPos = backgrounds[i].transform.position.x + speed;
+            float yPos = backgrounds[i].transform.position.y + speed;
+            Vector2 position = new Vector2(xPos, yPos);
+          
+            if (backgrounds[i].transform.position.x > -pivotPoint / 2)
+            {
+                position = new Vector2(pivotPoint,  pivotPoint);
+            }
+            backgrounds[i].transform.position = position;
+        }
+    }
+}
